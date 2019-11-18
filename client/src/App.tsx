c46bc36faa445e883180af2b261e12b5facfae0c
eemil <eemil@pop-os.localdomain>
2019-11-18 14:53:54 +0200

    added navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import MainPage from "./components/public/Blogs/MainPage";
 import SingleBlogPage from "./components/public/Blogs/SingleBlogPage";
-import {
-    BrowserRouter as Router,
-    Route,
-    Redirect,
-    Switch
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/public/Layout/Navbar";
 import "./styles.css";
 
 const App: React.FC = () => {
     return (
         <Router>
+            <Navbar />
             <Switch>
                 <Route exact path="/" render={() => <MainPage />} />
                 <Route
