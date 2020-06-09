c47c4193d737e6b2b3a7c0f66e86ced45124c7dc
nireo <eemil.lehtikangas1@gmail.com>
2020-06-09 22:23:19 +0300

    removed redundant front-end files

--- client/src/components/public/Blogs/Editor/Toolbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {
-    children: React.ReactNode;
-};
-
-export const Toolbar: React.FC<Props> = ({ children }) => {
-    return <div className="format-toolbar">{children}</div>;
-};
