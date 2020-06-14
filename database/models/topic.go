80d6c8237391a705f975d8e947ab7f0bb7798147
nireo <eemil.lehtikangas1@gmail.com>
2020-06-14 16:23:57 +0300

    added more topic model functions

--- database/models/topic.go
+++ database/models/topic.go
@@ -25,6 +25,72 @@ func SerializeTopics(topics []Topic) []common.JSON {
 	return serializedTopics
 }
 
+// GetTopicWithID returns a topic with given id
+func GetTopicWithID(id string) (Topic, bool) {
+	db := common.GetDatabase()
+	var topic Topic
+	if err := db.Where("uuid = ?", id).First(&topic).Error; err != nil {
+		return topic, false
+	}
+
+	return topic, true
+}
+
+// GetTopicWithURL returns a topic with given url
+func GetTopicWithURL(url string) (Topic, bool) {
+	db := common.GetDatabase()
+	var topic Topic
+	if err := db.Where("url = ?", url).First(&topic).Error; err != nil {
+		return topic, false
+	}
+
+	return topic, true
+}
+
+// GetTopicWithTitle returns a topic with given title
+func GetTopicWithTitle(title string) (Topic, bool) {
+	db := common.GetDatabase()
+	var topic Topic
+	if err := db.Where("title = ?", title).First(&topic).Error; err != nil {
+		return topic, false
+	}
+
+	return topic, true
+}
+
+// GetPostsRelatedToTopic finds all posts in the topic's category
+func GetPostsRelatedToTopic(topic Topic) ([]Post, bool) {
+	db := common.GetDatabase()
+	var posts []Post
+	if err := db.Model(&topic).Related(&posts).Error; err != nil {
+		return posts, false
+	}
+
+	return posts, true
+}
+
+// GetAllTopics gets all in the database
+func GetAllTopics() ([]Topic, bool) {
+	db := common.GetDatabase()
+	var topics []Topic
+	if err := db.Find(&topics).Error; err != nil {
+		return topics, false
+	}
+
+	return topics, true
+}
+
+// GetAllUsersTopics returns all the topics created by the given user
+func GetAllUsersTopics(user User) ([]Topic, bool) {
+	db := common.GetDatabase()
+	var topics []Topic
+	if err := db.Model(&user).Related(&topics).Error; err != nil {
+		return topics, false
+	}
+
+	return topics, true
+}
+
 // Serialize formats topic to JSON-format
 func (t *Topic) Serialize() common.JSON {
 	return common.JSON{
