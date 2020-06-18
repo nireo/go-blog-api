5b36b3c02fac49cc57e108e4685af02be4b47104
nireo <eemil.lehtikangas1@gmail.com>
2020-06-18 21:05:10 +0300

    created latest and most popular topic pages

--- client/src/components/public/Blogs/Topic/Latest.tsx
+++ client/src/components/public/Blogs/Topic/Latest.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { TopicWithPosts } from '../../../../interfaces/topic.interfaces';
+import { Post } from '../../../../interfaces/post.interfaces';
+import Blog from '../Blog';
 
-export const Latest = () => {
-  return <div></div>;
+type Props = {
+  topic: TopicWithPosts;
+};
+
+export const Latest: React.FC<Props> = ({ topic }) => {
+  return (
+    <div>
+      {topic.posts.slice(0, 3).map((post: Post) => (
+        <div key={`${post.id}`} style={{ marginBottom: '2rem' }}>
+          <Blog
+            id={String(post.id)}
+            likes={post.likes}
+            title={post.title}
+            description={post.description}
+            url={post.image_url}
+            uuid={post.uuid}
+            created={post.created_at}
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
--- client/src/components/public/Blogs/Topic/MostPopular.tsx
+++ client/src/components/public/Blogs/Topic/MostPopular.tsx
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { TopicWithPosts } from '../../../../interfaces/topic.interfaces';
+import { Post } from '../../../../interfaces/post.interfaces';
+import Blog from '../Blog';
 
-export const MostPopular: React.FC = () => {
-  return <div></div>;
+type Props = {
+  topic: TopicWithPosts;
+};
+
+const sortByLikes = (a: Post, b: Post) => {
+  if (a.likes < b.likes) {
+    return -1;
+  } else if (a.likes > b.likes) {
+    return 1;
+  }
+  return 0;
+};
+
+export const MostPopular: React.FC<Props> = ({ topic }) => {
+  const [sortedPosts, setSortedPosts] = useState<Post[]>([]);
+  const [loaded, setLoaded] = useState<boolean>(false);
+  useEffect(() => {
+    if (!loaded && sortedPosts.length === 0) {
+      setSortedPosts(topic.posts.sort(sortByLikes));
+    }
+  }, []);
+
+  return (
+    <div>
+      {sortedPosts.map((post: Post) => (
+        <div key={`${post.id}`} style={{ marginBottom: '2rem' }}>
+          <Blog
+            id={String(post.id)}
+            likes={post.likes}
+            title={post.title}
+            description={post.description}
+            url={post.image_url}
+            uuid={post.uuid}
+            created={post.created_at}
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
