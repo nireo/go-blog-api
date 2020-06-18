055c530c4f422e93e46275199f5e589cf291e8be
nireo <eemil.lehtikangas1@gmail.com>
2020-06-18 16:20:19 +0300

    added a notification reducer

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk';
 import userReducer from './user/reducer';
 import postReducer from './posts/reducer';
 import topicReducer from './topics/reducer';
+import notificationReducer from './notification/reducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
   post: postReducer,
   topic: topicReducer,
+  notification: notificationReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
---
+++ client/src/store/notification/reducer.ts
@@ -0,0 +1,40 @@
+import { Dispatch } from 'redux';
+
+const reducer = (state: Notification | null = null, action: any) => {
+  switch (action.type) {
+    case 'SET_NOTIFICATION':
+      return action.data;
+    case 'CLEAR_NOTIFICATION':
+      return null;
+    default:
+      return state;
+  }
+};
+
+export const setNotification = (
+  newNotification: Notification,
+  duration: number
+) => {
+  return async (dispatch: Dispatch) => {
+    dispatch({
+      type: 'SET_NOTIFICATION',
+      data: newNotification,
+    });
+
+    setTimeout(() => {
+      dispatch({
+        type: 'CLEAR_NOTIFICATION',
+      });
+    }, duration * 1000);
+  };
+};
+
+export const clearNotificationAction = () => {
+  return (dispatch: Dispatch) => {
+    dispatch({
+      type: 'CLEAR_NOTIFICATION',
+    });
+  };
+};
+
+export default reducer;
